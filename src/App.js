258a113191eda6f814b9a93b14ce5cc27d7908f7
LuizEscobarC <luiz_escobar11@hotmail.com>
2025-04-21 20:23:39 -0400

    fix: finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Dashboard from './pages/Dashboard/Dashboard';
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
+import Header from './components/common/Header/Header';
+import Footer from './components/common/Footer/Footer';
 
 
 function App() {
